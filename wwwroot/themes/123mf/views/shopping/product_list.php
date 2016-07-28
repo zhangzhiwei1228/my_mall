9928334b018d57b9d28a48f8670d16b8bf4db714
= <=>
2016-07-28 14:54:28 +0800

    将查询的商品分页

--- wwwroot/themes/123mf/views/shopping/product_list.php
+++ wwwroot/themes/123mf/views/shopping/product_list.php
@@ -44,38 +44,8 @@
 <body class="bgcolor">
     <?php include_once VIEWS.'inc/header_shop02.php'; ?>
     <!-- <p class="ibuy bgwhite"><em></em>我能购买</p> -->
-    <ul class="good-list product-list" style="<?php if(count($this->datalist)>0) {?>margin-bottom: 60px;<?php }?>">
-        <?php foreach($this->datalist as $row) { ?>
-            <?php $row = $row->_raw; ?>
-            <?php if(!$row['is_selling'] && $row['is_checked'] != 2) continue; ?>
-    	<li>
-            <a class="bgwhite" href="<?=$this->url('./detail?id='.$row['id'])?>">
-                <p class="pic">
-                    <?php if ($row['sup']['lt']) { ?><i class="sup-lt"><?=$row['sup']['lt']?></i><?php } ?>
-                    <?php if ($row['sup']['ld']) { ?><i class="sup-ld"><?=$row['sup']['ld']?></i><?php } ?>
-                    <?php if ($row['sup']['rt']) { ?><i class="sup-rt"><?=$row['sup']['rt']?></i><?php } ?>
-                    <?php if ($row['sup']['rd']) { ?><i class="sup-rd"><?=$row['sup']['rd']?></i><?php } ?>
-                    <img src="<?=$row['thumb']?> " width="100%" />
-                </p>
-                <p class="name"><?=$this->highlight($row['title'], $this->_request->q)?></p>
-                <div class="good-price">
-                    <?php if ($row['skus'][0]['point1'] > 0) { ?>
-                    <p class="text01">快乐积分：<span><?=$row['skus'][0]['point1']?></span>积分</p>
-                    <?php } ?>
-                    <?php if ($row['skus'][0]['point2'] > 0) { ?>
-                    <p class="text01">免费积分：<span><?=$row['skus'][0]['point2']?></span>积分</p>
-                    <?php } ?>
-                    <?php if ($row['skus'][0]['exts']['ext1']['cash']) { ?>
-                    <p class="text02">现金+免费积分：￥<span><?=$row['skus'][0]['exts']['ext1']['cash']?></span>+<span><?=$row['skus'][0]['exts']['ext1']['point']?></span>免费积分</p>
-                    <?php } ?>
-                    <?php if ($row['skus'][0]['exts']['ext2']['cash']) { ?>
-                    <p class="text03">现金+积分币：￥<span><span><?=$row['skus'][0]['exts']['ext2']['cash']?></span>+<span><span><?=$row['skus'][0]['exts']['ext2']['point']?></span>积分币</p>
-                    <?php } ?>
-                    <p class="text04">原价：￥<span class=""><?=$row['skus'][0]['market_price']?></span></p>
-                </div>
-            </a>
-        </li>
-    	<?php } ?>
+    <ul class="good-list product-list good-ajax-list" style="<?php if(count($this->datalist)>0) {?>margin-bottom: 60px;<?php }?>">
+
     </ul>
     <div class="clear"></div>
     <?php if ($this->_request->action != 'search') { ?>
@@ -120,6 +90,48 @@
 <script type="text/javascript">
     $('#cnzz_stat_icon_1259881631').hide();
     // $('.good-list li:even').css('marginRight','2%')
+</script>
+<script>
+    $(function(){
+        var flag=true;
+        var n=1;
+        var sbt = '<?php echo $this->_request->sbt?>';
+        var q = '<?php echo $this->_request->q?>';
+        var cid = '<?php echo $this->_request->cid?>';
+        $.ajax({
+            url:"/goods/getgoodlist/?page="+n+'&sbt='+sbt+'&q='+q+'&cid='+cid,
+            success:function(e){
+                $('.good-ajax-list').append(e);
+                n++;
+                setTimeout(function(){
+                    flag=true;
+                },500)
+            }
+        });
+        $(window).scroll(function(){
+            var H=$('body,html').height();
+            var h=$(window).height();
+            var t=$('body').scrollTop();
+            if(t>=H-h*1.1 && flag==true){
+                flag=false;
+                $.ajax({
+                    url:"/goods/getgoodlist/?page="+n+'&sbt='+sbt+'&q='+q+'&cid='+cid,
+                    success:function(e){
+                        if(!e) {
+                            flag = false ;
+                            $('.good-ajax-list').append('<p style="text-align: center">已加载完毕<p>');
+                            return false;
+                        }
+                        $('.good-ajax-list').append(e);
+                        n++;
+                        setTimeout(function(){
+                            flag=true;
+                        },300)
+                    }
+                })
+            }
+        });
+    });
 </script>
 </body>
 </html>
