6a02128b8d701cf0e6ff8e09773b447ceadfde38
= <=>
2016-08-01 15:03:53 +0800

    选择商品属性，提示剩余量

--- wwwroot/themes/123mf/views/shopping/good_detail.php
+++ wwwroot/themes/123mf/views/shopping/good_detail.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <head>
-<?php include_once VIEWS.'inc/head.php'; ?>
+	<?php include_once VIEWS.'inc/head.php'; ?>
 </head>
 <style type="text/css">
 	.good-price .text04 {
@@ -38,22 +38,22 @@
 		bottom: 5px;
 	}
 	.cheaper {
-	    padding: 0px 5px;
-	    background: #b40000;
-	    border-radius: 5px;
-	    color: #fff;
-	    font-size: 12px;
-	    margin-left: 5px;
+		padding: 0px 5px;
+		background: #b40000;
+		border-radius: 5px;
+		color: #fff;
+		font-size: 12px;
+		margin-left: 5px;
 	}
 </style>
 <body class="bgcolor">
 
-    <?php include_once VIEWS.'inc/header_shop02.php'; ?>
-    <div class="swiper-container bgwhite">
-	    <div class="swiper-wrapper">
-	        <?php $images = json_decode($this->data['ref_img'],1); $preview = $images[0]; ?>
-	    	<?php foreach((array)$images as $row) { ?>
-	        <div class="swiper-slide">
+<?php include_once VIEWS.'inc/header_shop02.php'; ?>
+<div class="swiper-container bgwhite">
+	<div class="swiper-wrapper">
+		<?php $images = json_decode($this->data['ref_img'],1); $preview = $images[0]; ?>
+		<?php foreach((array)$images as $row) { ?>
+			<div class="swiper-slide">
 				<p class="pic">
 					<?php if ($this->sup['lt']) { ?><i class="sup-lt"><?=$this->sup['lt']?></i><?php } ?>
 					<?php if ($this->sup['ld']) { ?><i class="sup-ld"><?=$this->sup['ld']?></i><?php } ?>
@@ -62,101 +62,106 @@
 					<img src="<?=$this->baseUrl($row['src'])?> " width="100%" />
 				</p>
 			</div>
-			<?php } ?>
-	    </div>
-	    <!-- 如果需要分页器 -->
-	    <div class="swiper-pagination"></div>  
+		<?php } ?>
 	</div>
-	<p class="product-name bgwhite"><?=$this->data['title']?></p>
-	<form method="post" class="bgwhite buyform" action="<?=$this->url('cart/add')?>">
-		<input type="hidden" name="goods_id" value="<?=$this->data['id']?>">
-		<input type="hidden" name="sku_id" value="<?=$this->sku['id']?>">
-		<input type="hidden" name="shipping_id" value="<?=$this->data['shipping_id']?>">
-		<input type="hidden" name="buynow" value="">
-		<input type="hidden" name="is_vip" id="is_vip" value="<?php echo $this->user['is_vip']?>" />
-		<div class="boline">
-			<div class="discount-box w90 f-cb">
-				<span class="discount-price fl">促销价:</span>
-				<label class="fl">
-					<?php if ($this->data->skus[0]['point1']) { ?>
+	<!-- 如果需要分页器 -->
+	<div class="swiper-pagination"></div>
+</div>
+<p class="product-name bgwhite"><?=$this->data['title']?></p>
+<form method="post" class="bgwhite buyform" action="<?=$this->url('cart/add')?>">
+	<input type="hidden" name="goods_id" id="goods_id" value="<?=$this->data['id']?>">
+	<input type="hidden" name="sku_id" id="sku_id" value="">
+	<input type="hidden" name="shipping_id" value="<?=$this->data['shipping_id']?>">
+	<input type="hidden" name="buynow" value="">
+	<input type="hidden" name="is_vip" id="is_vip" value="<?php echo $this->user['is_vip']?>" />
+	<input type="hidden" name="spec" id="spec" value="<?=$this->sku['id'] ?>" />
+	<div class="boline">
+		<div class="discount-box w90 f-cb">
+			<span class="discount-price fl">促销价:</span>
+			<label class="fl">
+				<?php if ($this->data->skus[0]['point1']) { ?>
 					<p class="select"><label><input type="radio" name="price_type" value="1"  />&nbsp;&nbsp;<?=$this->data->skus[0]['point1']?> 快乐积分</label></p>
-					<?php } ?>
-					<?php if ($this->data->skus[0]['point2']) { ?>
+				<?php } ?>
+				<?php if ($this->data->skus[0]['point2']) { ?>
 					<p class="select"><label><input type="radio" name="price_type" value="2" />&nbsp;&nbsp;<?=$this->data->skus[0]['point2']?> 免费积分</label></p>
-					<?php } ?>
-					<?php if ($this->data->skus[0]['exts']['ext1']['cash']) { ?>
+				<?php } ?>
+				<?php if ($this->data->skus[0]['exts']['ext1']['cash']) { ?>
 					<p class="select"><label><input type="radio" name="price_type" value="3" />&nbsp;&nbsp;<?=$this->data->skus[0]['exts']['ext1']['cash']?> 元 + <?=$this->data->skus[0]['exts']['ext1']['point']?> 免费积分</label></p>
-					<?php } ?>
-					<?php if ($this->data->skus[0]['exts']['ext2']['cash']) { ?>
+				<?php } ?>
+				<?php if ($this->data->skus[0]['exts']['ext2']['cash']) { ?>
 					<p class="select"><label><input type="radio" name="price_type" value="4" />&nbsp;&nbsp;<?=$this->data->skus[0]['exts']['ext2']['cash']?> 元 + <?=$this->data->skus[0]['exts']['ext2']['point']?> 积分币</label></p>
-					<?php } ?>
-				</label>
-				<div class="nnex f-cb">
-					<p class="original fl" style="width:auto;">原  价:&nbsp;&nbsp;<span>¥<?=$this->data->skus[0]['market_price']?> 元</span></p>
-					<strong class="cheaper fl"><?=$this->data['notes']?></strong>
-					<p class="selled fr">已售  <?=$this->data['sales_num']?>件</p>
-				</div>
+				<?php } ?>
+			</label>
+			<div class="nnex f-cb">
+				<p class="original fl" style="width:auto;">原  价:&nbsp;&nbsp;<span>¥<?=$this->data->skus[0]['market_price']?> 元</span></p>
+				<strong class="cheaper fl"><?=$this->data['notes']?></strong>
+				<p class="selled fr">已售  <?=$this->data['sales_num']?>件</p>
 			</div>
 		</div>
-		<!-- <div class="boline">
-			<div class="send-addess w90">
-				<select class="fl">
-					<option>配送至浙江省</option>
-					<option>xxxx</option>
-					</option>
-				</select>
-				<p class="fl postage">邮费  10元</p>
-				<p class="fr">A类综合打包</p>
-				<div class="clear"></div>
-			</div>
-		</div> -->
-		<div class="boline">
-			<div class="buy-muns w90">
-				<span class="title fl">购买数量：</span>
-				<div class="click-nums fl">
-					<a href="javascript:void(0);" class="jian">-</a>
-		            <input type="text" name="quantity" value="1">
-					<a href="javascript:void(0);" class="add">+</a>
-				</div>
-				<span class="fl">&nbsp;&nbsp;&nbsp;&nbsp;库存 <?=$this->data['quantity']?>件</span>
-				<div class="clear"></div>
-			</div>
-		</div>
-		<?php
-		$opts = $this->data->getSkuOpts();
-		foreach((array)$opts as $row) { ?>
+	</div>
+	<!-- <div class="boline">
+        <div class="send-addess w90">
+            <select class="fl">
+                <option>配送至浙江省</option>
+                <option>xxxx</option>
+                </option>
+            </select>
+            <p class="fl postage">邮费  10元</p>
+            <p class="fr">A类综合打包</p>
+            <div class="clear"></div>
+        </div>
+    </div> -->
+
+	<?php
+	$opts = $this->data->getSkuOpts();
+	foreach((array)$opts as $row) { ?>
 		<div class="boline">
 			<div class="good-style w90">
 				<p class="style-name"><?=$row['name']?></p>
 				<ul>
 					<?php
 					foreach($row['values'] as $k => $v) { ?>
-					<li><a href="javascript:;" data-param="<?=$row['name']?>:<?=$v?>" onclick="$.buychoose(this)"></a><?=$v?></li>
+						<li><a href="javascript:;" data-param="<?=$row['name']?>:<?=$v?>" onclick="$.buychoose(this)"></a><?=$v?></li>
 					<?php } ?>
 				</ul>
 				<div class="clear"></div>
 			</div>
 		</div>
-		<?php } ?>
-	</form>
-	<div class="product-details-box bgwhite">
-		<p class="product-headline"><em class="fl"></em>&nbsp;&nbsp;&nbsp;&nbsp;<span class="cn">产品详情</span>&nbsp;&nbsp;<span class="en"></span>Introduction&nbsp; details</span></p>.
-		<div class="time167">
-			<p>测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字测试文字</p>
+	<?php } ?>
+	<div class="boline">
+		<div class="buy-muns w90">
+			<span class="title fl">购买数量：</span>
+			<div class="click-nums fl">
+				<a href="javascript:void(0);" class="jian">-</a>
+				<input type="text" name="quantity" value="1">
+				<a href="javascript:void(0);" class="add">+</a>
+			</div>
+			<span>&nbsp;&nbsp;当前库存 <label class="sku_boline" style="color: red"><?php echo $this->sku['spec'] ? '0' : $this->sku['quantity']?></label>件</span>
+			<span >&nbsp;&nbsp;总库存 <?=$this->data['quantity']?>件</span>
+			<div class="clear"></div>
 		</div>
-		<p class="product-headline"><em class="fl"></em>&nbsp;&nbsp;&nbsp;&nbsp;<span class="cn">产品详情</span>&nbsp;&nbsp;<span class="en"></span>Product&nbsp; details</span></p>.
-		<p class="pic"><?=$this->data['description']?></p>
 	</div>
-	<div class="end163" style="width: 204px;margin-left: -92px">
-		请选择分类
+</form>
+<div class="product-details-box bgwhite">
+	<?php if($this->data['summary']) {?>
+	<p class="product-headline"><em class="fl"></em>&nbsp;&nbsp;&nbsp;&nbsp;<span class="cn">商品描述</span>&nbsp;&nbsp;<span class="en"></span>Introduction&nbsp; details</span></p>.
+	<div class="time167">
+		<p><?=$this->data['summary']?></p>
 	</div>
-	<input type="hidden" value="<?php echo $_SESSION['login_user_id']?>" id="is_login"/>
-    <!--<?php //include_once VIEWS.'inc/footer_shopping.php'; ?>-->
-    <?php //include_once VIEWS.'inc/footer01.php'; ?>
-    <?php include_once VIEWS.'inc/footer_buying.php'; ?>
+	<?php }?>
+	<p class="product-headline"><em class="fl"></em>&nbsp;&nbsp;&nbsp;&nbsp;<span class="cn">产品详情</span>&nbsp;&nbsp;<span class="en"></span>Product&nbsp; details</span></p>.
+	<p class="pic"><?=$this->data['description']?></p>
+</div>
+<div class="end163" style="width: 204px;margin-left: -92px">
+	请选择分类
+</div>
+<input type="hidden" value="<?php echo $_SESSION['login_user_id']?>" id="is_login"/>
+<!--<?php //include_once VIEWS.'inc/footer_shopping.php'; ?>-->
+<?php //include_once VIEWS.'inc/footer01.php'; ?>
+<?php include_once VIEWS.'inc/footer_buying.php'; ?>
 <?php
-	echo static_file('m/js/main.js');
-	
+echo static_file('m/js/main.js');
+
 ?>
 
 
@@ -164,14 +169,31 @@
 
 	function limit(){
 		var avId = new Array();
+		var good_id = $('#goods_id').val();
 		$(".bgwhite ul li").each(function(i,el){
 			var $el = $(el);
 			if ($el.hasClass('goodstylecurr')) {
-				avId.push( $el.data("id"));
+				avId.push("[" + $(this).children().attr("data-param")+"]");
 			};
-		})
+		});
 		var str_avId=avId.join();
-		// alert(str_avId)
+		$.get('/goods/getgoodsku/', {param:str_avId,good_id:good_id}, function(data){
+			var quantity = data.quantity;
+			if(quantity !=0 && quantity != 'error') {
+				$('.sku_boline').text(data.quantity);
+				$('#sku_id').val(data.sku_id);
+			} else {
+				if(quantity == 'error') {
+					$('.sku_boline').text(0);
+				} else {
+					$('.sku_boline').text(0);
+					$(".end163").show().text('所选已经售空，请选择其他规格的').css('font-size','12px');
+					setTimeout(function(){
+						$('.end163').hide();
+					},1000);
+				}
+			}
+		},'json');
 	}
 
 
@@ -189,35 +211,36 @@
 
 <script type="text/javascript">
 	var mySwiper = new Swiper ('.swiper-container', {
-    loop: true,
-    
-    // 如果需要分页器
-    pagination: '.swiper-pagination',
-    autoplay: 2000
-    
-  	});
+		loop: true,
+
+		// 如果需要分页器
+		pagination: '.swiper-pagination',
+		autoplay: 2000
+
+	});
 	$('.discount-box label .select label').click(function(){
-	 	$(this).addClass('selectcurr').siblings('.select').removeClass('selectcurr');
-	 	$(this).css("color","#fc0000");
-	 	
-	 	$(this).parents("p").siblings('p').find('label').css("color","#777");
+		$(this).addClass('selectcurr').siblings('.select').removeClass('selectcurr');
+		$(this).css("color","#fc0000");
+
+		$(this).parents("p").siblings('p').find('label').css("color","#777");
 	});
 
-  	$('.good-style ul li').click(function(){
-  		$(this).addClass('goodstylecurr').siblings('li').removeClass('goodstylecurr');
-  		limit();
-  	});
+	$('.good-style ul li').click(function(){
+		$(this).addClass('goodstylecurr').siblings('li').removeClass('goodstylecurr');
+		limit();
+	});
 
-  	$('input:radio[name="price_type"]').click(function(){
+	$('input:radio[name="price_type"]').click(function(){
 		$(".bgwhite .boline").eq(0).css("border","0px none");
 		$(".bgwhite .boline").eq(0).css("border-bottom","1px solid #f1f0f0");
-  	});
+	});
 
 	$(".click-buy").click(function(){
 		var val=$('input:radio[name="price_type"]:checked').val();
 		var flag = 1;
 		var is_vip = $('#is_vip').val();
 		var is_login = $('#is_login').val();
+		var quantity = $('.sku_boline').text();
 		if(!is_login) {
 			flag = 0;
 			$(".end163").show().text('您还没有登录，请先登录');
@@ -234,141 +257,156 @@
 			},3000);
 			return false;
 		}
-
+		if(quantity == 0) {
+			flag = 0;
+			$(".end163").show().text('当前规格已售空');
+			setTimeout(function(){
+				$(".end163").hide();
+			},3000);
+			return false;
+		}
 		if(val==null){
 			$("html,body").stop().animate({scrollTop:$(".bgwhite .boline").eq(0).offset().top - 90});
 			$(".bgwhite .boline").eq(0).css("border","2px solid #fc0000");
-			 flag = 0;
+			flag = 0;
 		} else {
 			if($("html,body").hasClass('.style-name')) {
 				$("html,body").stop().animate({scrollTop:$(".style-name").eq(0).offset().top - 90});
 			}
 			//$("html,body").stop().animate({scrollTop:$(".style-name").eq(0).offset().top - 90});
-			 flag = 1;
+			flag = 1;
 		}
 
-  	$(".good-style ul").each(function(){
-  			if ($(this).children('li').hasClass('goodstylecurr')) {
-  		}else{
-  			$(this).parents(".boline").css("border","2px solid #fc0000");
-  			setTimeout(function(){
-  				$(this).css("border","0px none");
-  			},3000);
-  			flag = 0;
-  			$(".end163").show();
-  			setTimeout(function(){
-  				$(".end163").hide();
-  			},3000);
-  			flag = 0;
-  		}
-  	});
+		$(".good-style ul").each(function(){
+			if ($(this).children('li').hasClass('goodstylecurr')) {
+			}else{
+				$(this).parents(".boline").css("border","2px solid #fc0000");
+				setTimeout(function(){
+					$(this).css("border","0px none");
+				},3000);
+				flag = 0;
+				$(".end163").show();
+				setTimeout(function(){
+					$(".end163").hide();
+				},3000);
+				flag = 0;
+			}
+		});
 
-  	if (flag) {
-  		$('[name=buynow]').val(1);
-		<?php
-			$_SESSION['pay_confirm_login'] = true;
-			$_SESSION['pay_confirm_login_url'] = true;
-		?>
-		$('.buyform').submit();
-	}
-
-	
- 	//$('.buyform').submit();
-  })
-
-$(function(){
-
-  	// $('.buy-muns .click-nums .add').click(function(){
-   //      var $this = $(this),
-   //          $input = $this.parent().find('input'),
-
-   //          num = parseInt($input.val());
-   //      $input.val(num+1);
-
-   //    })
-
-   //  $('.buy-muns .click-nums .jian').click(function(){
-   //      var $this = $(this),
-   //          $input = $this.parent().find('input'),
-   //          num = parseInt($input.val());
-   //      if(num > 1){
-   //          $input.val(num-1);
-
-   //      }else{
-   //          $input.val(1);
-
-   //      }
-   //  })
-
-   //  $('.click-nums input').keyup(function() {
-   //      var n = parseInt($(this).parent().find("input").val());
-   //      if(n > 1){
-   //          $(this).parent().find("input").val(n);
-
-   //      }else{
-   //          $(this).parent().find("input").val(1);
-
-   //      }
-   //  });
-});
-
-$(".btn-addcart").click(function(){
-	var flag = 1;
-	var val=$('input:radio[name="price_type"]:checked').val();
-	var is_vip = $('#is_vip').val();
-	var is_login = $('#is_login').val();
-	if(!is_login) {
-		flag = 0;
-		$(".end163").show().text('您还没有登录，请先登录');
-		setTimeout(function(){
-			window.location.href='<?php echo site_url('usercp/passport/login/');?>';
-		},3000);
-		return false;
-	}
-	if(is_vip != 1) {
-		flag = 0;
-		$(".end163").show().text('您还没有激活，请先激活');
-		setTimeout(function(){
-			window.location.href='<?php echo site_url('usercp/vip/');?>';
-		},3000);
-		return false;
-	}
-
-	if(val==null){
-		$("html,body").stop().animate({scrollTop:$(".bgwhite .boline").eq(0).offset().top - 90});
-		$(".bgwhite .boline").eq(0).css("border","2px solid #fc0000");
-		flag = 0;
-	} else {
-		if($("html,body").hasClass('.style-name')) {
-			$("html,body").stop().animate({scrollTop:$(".style-name").eq(0).offset().top - 90});
+		if (flag) {
+			$('[name=buynow]').val(1);
+			<?php
+                $_SESSION['pay_confirm_login'] = true;
+                $_SESSION['pay_confirm_login_url'] = true;
+            ?>
+			$('.buyform').submit();
 		}
-		flag = 1;
-	}
-	$(".good-style ul").each(function(){
-		if ($(this).children('li').hasClass('goodstylecurr')) {
-		}else{
-			$(this).parents(".boline").css("border","2px solid #fc0000");
-			setTimeout(function(){
-				$(this).css("border","0px none");
-			},3000);
+
+
+		//$('.buyform').submit();
+	})
+
+	$(function(){
+
+		// $('.buy-muns .click-nums .add').click(function(){
+		//      var $this = $(this),
+		//          $input = $this.parent().find('input'),
+
+		//          num = parseInt($input.val());
+		//      $input.val(num+1);
+
+		//    })
+
+		//  $('.buy-muns .click-nums .jian').click(function(){
+		//      var $this = $(this),
+		//          $input = $this.parent().find('input'),
+		//          num = parseInt($input.val());
+		//      if(num > 1){
+		//          $input.val(num-1);
+
+		//      }else{
+		//          $input.val(1);
+
+		//      }
+		//  })
+
+		//  $('.click-nums input').keyup(function() {
+		//      var n = parseInt($(this).parent().find("input").val());
+		//      if(n > 1){
+		//          $(this).parent().find("input").val(n);
+
+		//      }else{
+		//          $(this).parent().find("input").val(1);
+
+		//      }
+		//  });
+	});
+
+	$(".btn-addcart").click(function(){
+		var flag = 1;
+		var val=$('input:radio[name="price_type"]:checked').val();
+		var is_vip = $('#is_vip').val();
+		var is_login = $('#is_login').val();
+		var quantity = $('.sku_boline').text();
+		if(!is_login) {
 			flag = 0;
-			$(".end163").show();
+			$(".end163").show().text('您还没有登录，请先登录');
+			setTimeout(function(){
+				window.location.href='<?php echo site_url('usercp/passport/login/');?>';
+			},3000);
+			return false;
+		}
+		if(is_vip != 1) {
+			flag = 0;
+			$(".end163").show().text('您还没有激活，请先激活');
+			setTimeout(function(){
+				window.location.href='<?php echo site_url('usercp/vip/');?>';
+			},3000);
+			return false;
+		}
+		if(quantity == 0) {
+			flag = 0;
+			$(".end163").show().text('当前规格已售空');
 			setTimeout(function(){
 				$(".end163").hide();
 			},3000);
+			return false;
+		}
+		if(val==null){
+			$("html,body").stop().animate({scrollTop:$(".bgwhite .boline").eq(0).offset().top - 90});
+			$(".bgwhite .boline").eq(0).css("border","2px solid #fc0000");
 			flag = 0;
+		} else {
+			if($("html,body").hasClass('.style-name')) {
+				$("html,body").stop().animate({scrollTop:$(".style-name").eq(0).offset().top - 90});
+			}
+			flag = 1;
+		}
+		$(".good-style ul").each(function(){
+			if ($(this).children('li').hasClass('goodstylecurr')) {
+			}else{
+				$(this).parents(".boline").css("border","2px solid #fc0000");
+				setTimeout(function(){
+					$(this).css("border","0px none");
+				},3000);
+				flag = 0;
+				$(".end163").show();
+				setTimeout(function(){
+					$(".end163").hide();
+				},3000);
+				flag = 0;
+			}
+		});
+
+		if (flag) {
+			$('[name=buynow]').val(0);
+			$('.buyform').submit();
 		}
 	});
 
-  	if (flag) {
-  		$('[name=buynow]').val(0);
-  		$('.buyform').submit();
-	}
-  });
-
-$(".good-style ul li").click(function(){
-	$(this).parents(".boline").css("border","0px none")
-})
+	$(".good-style ul li").click(function(){
+		$(this).parents(".boline").css("border","0px none")
+	})
 
 </script>
 </body>
