4ae3c084f634dbaf8ff76a5a9498039bdfc89ad5
= <=>
2016-10-09 15:05:20 +0800

    商家积分流水

--- wwwroot/themes/123mf/views/merchants.php
+++ wwwroot/themes/123mf/views/merchants.php
@@ -62,8 +62,8 @@
 
 	<?php if ($this->user['role'] == 'seller') { ?>
 	<div style="margin-bottom:71px;" class="member-rank bgwhite">
-		<a href="<?php echo $this->url('agent/logs/recharge')?>" ><p class="w90">商家本日充值免费积分：<?php echo $this->employ['total']?>（查看详情）</p></a>
-		<a href="<?php echo $this->url('agent/logs/employ')?>" ><p class="w90">商家本日赠送免费积分：<?php echo $this->recharge['total']?>（查看详情）</p></a>
+		<a href="<?php echo $this->url('agent/logs/recharge')?>" ><p class="w90">商家本日充值免费积分：<?php echo $this->employ['total'] ? $this->employ['total'] : 0 ?>（查看详情）</p></a>
+		<a href="<?php echo $this->url('agent/logs/employ')?>" ><p class="w90">商家本日赠送免费积分：<?php echo $this->recharge['total'] ? $this->recharge['total'] : 0?>（查看详情）</p></a>
 
 
 		<!--<p class="w90">一级会员：我直接邀请注册的会员</p>
