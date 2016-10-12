729733cc4d16d23454503e08b560b27d41a1a0e1
= <=>
2016-10-12 15:56:27 +0800

    后台显示运费

--- services/default/CartController.php
+++ services/default/CartController.php
@@ -135,7 +135,8 @@ class CartController extends Controller_Action
 				$shippings[$key] = $item['shipping_id'];
 			}
 
-
+			$postage = $this->doPostAge($item, $item['total'], $item['subtotal_weight']);
+			$status['total_freight'] = $postage;
 			$oid = M('Order')->insert(array_merge($_POST, $status, array(
 				'code' => time(),
 				'buyer_id' => $buyer->id,
