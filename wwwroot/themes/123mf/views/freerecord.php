8ec9752603bd208d00fe3354c6a8678e4477db89
= <=>
2016-10-14 11:44:14 +0800

    支付成功后跳转再跳转的问题

--- wwwroot/themes/123mf/views/freerecord.php
+++ wwwroot/themes/123mf/views/freerecord.php
@@ -64,7 +64,7 @@
 <body>
 	<div class="n-rechargerecord">
 		<div class="n-personal-center-tit">
-			<a href="javascript:history.go(-1);"><img width="11" height="21" src="<?php echo static_file('mobile/img/img-22.png'); ?> " alt=""></a>
+			<a href="<?php echo $this->type == 'worth_gold' ? $this->url('/usercp/money') : 'javascript:history.go(-1);' ?>"><img width="11" height="21" src="<?php echo static_file('mobile/img/img-22.png'); ?> " alt=""></a>
 			<?php
 				switch($this->type) {
 					case 'credit' :
