aac8e13f73e8c5a44846e9d0f4b0865391fc2d1f
zhangzhiwei <zhangzhiwei1228@gmail.com>
2016-11-25 11:57:10 +0800

    调试接口

--- services/app/IndexController.php
+++ services/app/IndexController.php
@@ -98,12 +98,12 @@ class App_IndexController extends App_Controller_Action
                 foreach($shops as &$shop){
                     $shop['thumb'] = 'http://'.$_SERVER['HTTP_HOST'].$shop['thumb'];
                 }
-                var_dump($key);
                 $data[$key]['shop'] = $shops ;
                 $data[$key]['name'] = $row['name'];
                 $data[$key]['is_special'] = $key;
             }
         }
+        print_r($data);
         //echo $this->_encrypt_data($data);
         echo $this->show_data($this->_encrypt_data($data));
         die();
