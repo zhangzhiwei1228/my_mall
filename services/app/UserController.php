383ddd9e9ed5ebd371af25a9418a3461d33a039e
zhangzhiwei <zhangzhiwei1228@gmail.com>
2016-12-12 16:29:40 +0800

    测试接口

--- services/app/UserController.php
+++ services/app/UserController.php
@@ -327,7 +327,7 @@ class App_UserController extends App_Controller_Action
             ->leftJoin(M('Goods_Sku')->getTableName().' AS gsk', 'c.sku_id = gsk.id')
             ->columns('c.id,c.goods_id,c.price_type,c.qty, gs.title,gs.thumb, gsk.point1, gsk.point2,gsk.point3,gsk.point4,gsk.point5,gsk.exts,gsk.spec')
             ->where('c.user_id ='.(int)$this->user->id)
-            ->order('c.create_time asc')
+            ->order('gsk.create_time asc')
             ->fetchRows()->toArray();
         foreach($carts as $key1=> &$row) {
             $row = M('User_Cart')->price_type($row);
