18bea70d615d5553712cbe618de495d3cd0b9247
zhangzhiwei <zhangzhiwei1228@gmail.com>
2016-12-15 16:53:21 +0800

    后台设置app跳转

--- appdata/conf/app.conf.php
+++ appdata/conf/app.conf.php
@@ -5,3 +5,17 @@
  * Date: 16-12-15
  * Time: 下午4:28
  */
+return array(
+    'advertise' => array(
+        array(
+            'name' => '商家详情',
+            'type' => 1
+        ),
+        array(),
+        array(),
+        array(),
+    ),
+    'jpush' => array(
+
+    )
+);
