c50a936352aaf2e721389f34e62a5e522dafafc4
zhangzhiwei <zhangzhiwei1228@gmail.com>
2016-12-19 16:21:27 +0800

    申请时验证手机号

--- services/app/VipController.php
+++ services/app/VipController.php
@@ -38,10 +38,43 @@ class App_VipController extends App_Controller_Action
         $area_id = $this->_request->area_id;//区id
         $address = $this->_request->address;//详细地址
         $contact = $this->_request->contact;//联系人
-        $phone = $this->_request->phone;//联系电话
+        $phone = $this->user->mobile;//联系电话
         $remark = $this->_request->remark;//留言
         if( !$type || !$phone || !$address) {
             echo self::_error_data(API_MISSING_PARAMETER,'缺少必要参数');
+            die();
+        }
+        if($type < 5) {
+            $where = 'mobile = '.$phone.' and role = '."'resale'".' and resale_grade = '.$type;
+        } elseif($type == 5) {
+            $where = 'mobile = '.$phone.' and role = '."'seller'";
+        } else {
+            $where = 'mobile = '.$phone.' and role = '."'agent'";
+        }
+        $result = M('User')->select('id')->where($where)->fetchRow()->toArray();
+        $msg = '';
+        switch($type) {
+            case 1:
+                $msg = '此手机号已经申请一星分销商，请不要重复申请';
+                break;
+            case 2:
+                $msg = '此手机号已经申请二星分销商，请不要重复申请';
+                break;
+            case 3:
+                $msg = '此手机号已经申请三星分销商，请不要重复申请';
+                break;
+            case 4:
+                $msg = '此手机号已经申请四星分销商，请不要重复申请';
+                break;
+            case 5:
+                $msg = '此手机号已经申请商家，请不要重复申请';
+                break;
+            case 6:
+                $msg = '此手机号已经申请代理商，请不要重复申请';
+                break;
+        }
+        if($result) {
+            echo self::_error_data(API_RESOURCES_NOT_FOUND,$msg);
             die();
         }
         $data = array(
