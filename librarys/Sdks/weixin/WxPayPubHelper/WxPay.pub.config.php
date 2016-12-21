a4fde2d62021cc61105736386675046007215a17
zhangzhiwei <zhangzhiwei1228@gmail.com>
2016-12-21 20:09:31 +0800

    微信回调

--- librarys/Sdks/weixin/WxPayPubHelper/WxPay.pub.config.php
+++ librarys/Sdks/weixin/WxPayPubHelper/WxPay.pub.config.php
@@ -7,26 +7,26 @@ class WxPayConf_pub
 {
 	//=======【基本信息设置】=====================================
 	//微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
-	const APPID = 'wxbaf288ecd80def6f';
+	const APPID = 'wx3f393bd1daf97d13';
 	//受理商ID，身份标识
-	const MCHID = '1248764901';
+	const MCHID = '1420980902';
 	//商户支付密钥Key。审核通过后，在微信发送的邮件中查看
-	const KEY = '48888888888888888888888888888886';
+	const KEY = 'eJxO3uehIINBsF8ngIsiDx6c2YfdmHie';
 	//JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
-	const APPSECRET = 'a30b0f6567b463964b581c5897ffa29a';
+	const APPSECRET = '5b1fff4b9064672254a28466868c78f1';
 	
 	//=======【JSAPI路径设置】===================================
 	//获取access_token过程中的跳转uri，通过跳转将code传入jsapi支付页面
-	const JS_API_CALL_URL = 'http://www.xxxxxx.com/demo/js_api_call.php';
+	const JS_API_CALL_URL = '';
 	
 	//=======【证书路径设置】=====================================
 	//证书路径,注意应该填写绝对路径
-	const SSLCERT_PATH = '/xxx/xxx/xxxx/WxPayPubHelper/cacert/apiclient_cert.pem';
-	const SSLKEY_PATH = '/xxx/xxx/xxxx/WxPayPubHelper/cacert/apiclient_key.pem';
+	const SSLCERT_PATH = '';
+	const SSLKEY_PATH = '';
 	
 	//=======【异步通知url设置】===================================
 	//异步通知url，商户根据实际开发过程设定
-	const NOTIFY_URL = 'http://www.xxxxxx.com/demo/notify_url.php';
+	const NOTIFY_URL = '';
 
 	//=======【curl超时设置】===================================
 	//本例程通过curl使用HTTP POST方法，此处可修改其超时时间，默认为30秒
