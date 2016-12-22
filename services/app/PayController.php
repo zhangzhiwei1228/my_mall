f7658b53d1975796522ad6d05831613756ff3f84
zhangzhiwei <zhangzhiwei1228@gmail.com>
2016-12-22 11:08:09 +0800

    微信支付宝签名和回调

--- services/app/PayController.php
+++ services/app/PayController.php
@@ -114,9 +114,10 @@ class App_PayController extends App_Controller_Action
         );
 
         $paydata['sign']=$notify->getSign($paydata);
-        $data['sign']=$notify->getSign($paydata);
-        echo $this->_encrypt_data($paydata);
-        //echo $this->show_data($this->_encrypt_data($paydata));
+        //$data['sign']=$notify->getSign($paydata);
+        $data = $notify->arrayToXml($paydata);
+        echo $this->_encrypt_data($data);
+        //echo $this->show_data($this->_encrypt_data($data));
         die();
         $xml= $notify->postXmlCurl($notify->arrayToXml($paydata),'https://api.mch.weixin.qq.com/pay/unifiedorder');
         $paydatanew=$notify->xmlToArray($xml);
