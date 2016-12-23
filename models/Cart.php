a821ba45f9017cb880471a3601f1713d9fb69f9f
zhangzhiwei <zhangzhiwei1228@gmail.com>
2016-12-23 15:59:04 +0800

    ceshi

--- models/Cart.php
+++ models/Cart.php
@@ -256,7 +256,7 @@ class Cart
 					->where('user_id ='. $uid . ' and goods_id ='.$item['id'].' and sku_id ='.$item['skuId'].' and shipping_id ='.$item['shipping_id'].' and price_type='.$item['priceType'])
 					->fetchRows()->toArray();
 				if($items) {
-					M('User_Cart')->update(array('qty'=>$item['qty']),'user_id ='. $uid . ' and goods_id ='.$item['id'].' and sku_id ='.$item['skuId'].' and shipping_id ='.$item['shipping_id'].' and price_type='.$item['priceType']);
+					$cart_id = M('User_Cart')->update(array('qty'=>$item['qty']),'user_id ='. $uid . ' and goods_id ='.$item['id'].' and sku_id ='.$item['skuId'].' and shipping_id ='.$item['shipping_id'].' and price_type='.$item['priceType']);
 				} else {
 					$cart_id = M('User_Cart')->insert(array(
 						'user_id' => $uid,
