2a6393faf678a9fd8ec489eb7c5b53edd93bd934
zhangzhiwei <zhangzhiwei1228@gmail.com>
2017-01-05 15:44:24 +0800

    判断分销商用户，有没申请过

--- services/app/UserController.php
+++ services/app/UserController.php
@@ -88,6 +88,13 @@ class App_UserController extends App_Controller_Action
             $data['resale_grade'] = $parent['role'];
         }
         $data['resale_grade'] = $data['resale_grade'] ? $data['resale_grade'] : 0;
+        if($user_data['role'] == 'resale' && $user_data['resale_grade'] < 4 && $user_data['resale_grade'] > 1) {
+            $resale_apply = M('Resale_Apply')->select('id')->where('phone ='.$user_data['mobile'].' and grade >'.$user_data['resale_grade'])->fetchRows()->toArray();
+            foreach($resale_apply as $k=>$v) {
+                $key = $k+$user_data['resale_grade']+1;
+                $data['is_apply'.$key] = $resale_apply ? 1 : 0;
+            }
+        }
         $data['count_cart'] = $count;
         //$data['exts'] = $extends;
         echo $this->_encrypt_data($data);
