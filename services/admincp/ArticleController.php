697849e1ec21ad7fb1bf5532dad0e052a196e80d
zhangzhiwei <zhangzhiwei1228@gmail.com>
2017-01-10 16:49:20 +0800

    dingdan tuisong

--- services/admincp/ArticleController.php
+++ services/admincp/ArticleController.php
@@ -83,13 +83,14 @@ class Admincp_ArticleController extends Admincp_Controller_Action
 		if ($this->_request->isPost()) {
 			$data = $this->_request->getPosts();
 
-			if($data['area_id'] && $data['category_id'] == 15 ) {
+			if($data['area_id'] && ($data['category_id'] == 15 || $data['category_id'] == 22) ) {
 
 				$ids = M('Region')->getChildIds((int)$data['area_id']);
 				$user_areas = M('User_Area')->select('user_id')->where('area_id IN (' . ($ids ? $ids : (int)$data['area_id']) . ')')->fetchRows()->toArray();
 				$user_ids = M('User_Area')->select('user_id')->fetchCols('user_id');
 				$user_ids = implode(',', $user_ids);
 				$users = M('User')->select('id')->where('id NOT IN (' . ($user_ids ? $user_ids : 0) . ')')->fetchRows()->toArray();
+				$artice_id = M('Article')->insert(array_merge($this->_request->getPosts(), $this->_request->getFiles()));
 				foreach ($users as $user1) {
 					M('Message')->insert(array(
 						'recipient_uid' => $user1['id'],
@@ -99,8 +100,16 @@ class Admincp_ArticleController extends Admincp_Controller_Action
 						'title' => $data['title'],
 						'create_time' => time()
 					));
+					$exts = array (
+						'title' => $data['title'],
+						'content' => strip_tags($data['content']),
+						'extras' => array(
+							'id' => $artice_id,
+							'type' => 1//1是消息2是新品3是新品发货
+						)
+					);
+					M('Jpush')->push($user1['id'],1,$exts);
 				}
-
 				foreach ($user_areas as $user2) {
 					M('Message')->insert(array(
 						'recipient_uid' => $user2['user_id'],
@@ -112,7 +121,7 @@ class Admincp_ArticleController extends Admincp_Controller_Action
 					));
 				}
 
-				M('Article')->insert(array_merge($this->_request->getPosts(), $this->_request->getFiles()));
+				//M('Article')->insert(array_merge($this->_request->getPosts(), $this->_request->getFiles()));
 			} else {
 				M('Article')->insert(array_merge($this->_request->getPosts(), $this->_request->getFiles()));
 			}
