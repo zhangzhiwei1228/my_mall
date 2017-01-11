011a70730ef60966c6869ad46e6b323afed3d901
zhangzhiwei <zhangzhiwei1228@gmail.com>
2017-01-11 11:50:51 +0800

    商品修改

--- models/Order.php
+++ models/Order.php
@@ -262,6 +262,7 @@ class Order extends Abstract_Model
 				if ($order->status == 2 && $order->buyer->exists()) {
 					if($order->total_pay_amount) {
 						var_dump("123123");
+						var_dump($order->total_pay_amount);
 						die();
 						$order->buyer->income('refund', $order->total_pay_amount, 'TS-'.$order->code, '订单取消，自动退款')->commit();
 					}
