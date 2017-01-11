c32e064983d54b37c3798f74759582e5244df5ad
zhangzhiwei <zhangzhiwei1228@gmail.com>
2017-01-11 15:17:47 +0800

    商品修改

--- services/app/GoodsController.php
+++ services/app/GoodsController.php
@@ -289,12 +289,13 @@ class App_GoodsController extends App_Controller_Action
         $param1 = '';
         if(strpos($param,',')) {
             $param1 = explode(',',$param);
-            $param1 = $param1[1].','.$param1[0];
+            $param = $param1[1].','.$param1[0];
         }
         $good_id = (int)$this->_request->good_id;
         $quantity = $sku_id = 0;
         $arrs = M('Goods_Sku')->select()
-            ->where('goods_id = '.$good_id.' and (reverse(spec) LIKE '. 'reverse("%'.$param.'%") )')
+            //->where('goods_id = '.$good_id.' and (reverse(spec) LIKE '. 'reverse("%'.$param.'%") or reverse(spec) LIKE '.'reverse("%'.$param1.'%")'.' )')
+            ->where('goods_id = '.$good_id.' and (reverse(spec) LIKE '. 'reverse("%'.$param.'%"))')
             ->fetchRows()->toArray();
         if(count($arrs)) {
             foreach ($arrs as $arr) {
@@ -305,8 +306,8 @@ class App_GoodsController extends App_Controller_Action
             $quantity = 'error';
         }
         $data = array('quantity'=>$quantity,'sku_id'=>$sku_id);
-        //echo $this->_encrypt_data($data);
-        echo $this->show_data($this->_encrypt_data($data));
+        echo $this->_encrypt_data($data);
+        //echo $this->show_data($this->_encrypt_data($data));
         die();
     }
     /**
