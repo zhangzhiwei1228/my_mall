764d4da8345a743c5da68596d9d9de37b1ad9463
zhangzhiwei <zhangzhiwei1228@gmail.com>
2017-01-12 11:25:56 +0800

    判断资产

--- services/default/GoodsController.php
+++ services/default/GoodsController.php
@@ -535,12 +535,14 @@ class GoodsController extends Controller_Action
 		$param1 = '';
 		if(strpos($param,',')) {
 			$param1 = explode(',',$param);
-			$param1 = $param1[1].','.$param1[0];
+			//$param1 = $param1[1].','.$param1[0];
+			$param = $param1[1].','.$param1[0];
 		}
 		$good_id = (int)$this->_request->good_id;
 		$quantity = $sku_id = 0;
 		$arrs = M('Goods_Sku')->select()
-			->where('goods_id = '.$good_id.' and (reverse(spec) LIKE '. 'reverse("%'.$param.'%") or reverse(spec) LIKE '.'reverse("%'.$param1.'%")'.' )')
+			//->where('goods_id = '.$good_id.' and (reverse(spec) LIKE '. 'reverse("%'.$param.'%") or reverse(spec) LIKE '.'reverse("%'.$param1.'%")'.' )')
+			->where('goods_id = '.$good_id.' and (reverse(spec) LIKE '. 'reverse("%'.$param.'%"))')
 			->fetchRows()->toArray();
 		if(count($arrs)) {
 			foreach ($arrs as $arr) {
